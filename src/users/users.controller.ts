dafcd2fd8671692b2dc37882ec0381dc7f953bdf
Noneku <dev.nassim.pro@gmail.com>
2025-12-18 15:31:03 +0100

    fix(HttpRequest) : Add HttpCode 201 - Create user

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -6,6 +6,7 @@ import {
   Patch,
   Param,
   Delete,
+  HttpCode,
 } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { CreateUserDto } from './dto/create-user.dto';
@@ -16,6 +17,7 @@ export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
   @Post()
+  @HttpCode(201)
   create(@Body() createUserDto: CreateUserDto) {
     return this.usersService.create(createUserDto);
   }
