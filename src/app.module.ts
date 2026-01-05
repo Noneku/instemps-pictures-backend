09fd88faf2b41ec0eb10702b12c6ed786409189e
Noneku <dev.nassim.pro@gmail.com>
2026-01-05 13:14:38 +0100

    feat(app): Add configuration Throttler

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,9 +5,30 @@ import { UsersModule } from './users/users.module';
 import { BcryptService } from './bcrypt/bcrypt.service';
 import { BcryptModule } from './bcrypt/bcrypt.module';
 import { AuthModule } from './auth/auth.module';
+import {ThrottlerModule} from "@nestjs/throttler";
 
 @Module({
-  imports: [UsersModule, BcryptModule, AuthModule],
+  imports: [
+      ThrottlerModule.forRoot([
+        {
+          name: 'short',
+          ttl: 1000,
+          limit: 3,
+        },
+        {
+          name: 'medium',
+          ttl: 10000,
+          limit: 20,
+        },
+        {
+          name: 'long',
+          ttl: 60000,
+          limit: 100,
+        }
+      ]),
+      UsersModule,
+    BcryptModule,
+    AuthModule],
   controllers: [AppController],
   providers: [AppService, BcryptService],
 })
