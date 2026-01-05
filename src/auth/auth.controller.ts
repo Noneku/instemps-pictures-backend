20f95137a536b35734a492e7896e177ca7f7220d
Noneku <dev.nassim.pro@gmail.com>
2026-01-05 13:16:44 +0100

    refactor(auth): Rename 'SignIn' to 'Login' + feat(auth): Add Throttler decorator

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -2,18 +2,31 @@ import { Body, Controller, Post, HttpCode, HttpStatus } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { LoginDto } from './dto/login.dto';
 import {RegisterDto} from "./dto/register.dto";
+import {Throttle} from "@nestjs/throttler";
 
 @Controller('auth')
 export class AuthController {
   constructor(private authService: AuthService) {}
 
+  @Throttle({
+    default: {
+      limit: 5,
+      ttl: 60000
+    }
+  })
   @HttpCode(HttpStatus.OK)
   @Post('login')
-  signIn(@Body() loginDto: LoginDto) {
-    return this.authService.signIn(loginDto);
+  login(@Body() loginDto: LoginDto) {
+    return this.authService.logIn(loginDto);
   }
 
-  @HttpCode(HttpStatus.OK)
+  @Throttle({
+    default: {
+      limit: 5,
+      ttl: 60000
+    }
+  })
+  @HttpCode(HttpStatus.CREATED)
   @Post('register')
   register(@Body() registerDto: RegisterDto) {
     return this.authService.register(registerDto);
