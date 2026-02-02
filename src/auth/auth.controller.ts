ac5895bc745d7ce11e4f4c0ce276596c785851f7
Noneku <dev.nassim.pro@gmail.com>
2026-02-02 16:36:10 +0100

    feat(Auth): Add feat for get stats of user currently connected

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,9 +1,19 @@
-import { Body, Controller, Post, HttpCode, HttpStatus } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Post,
+  HttpCode,
+  HttpStatus,
+  Req,
+  UseGuards,
+  Get,
+} from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { LoginDto } from './dto/login.dto';
 import {RegisterDto} from "./dto/register.dto";
 import {Throttle} from "@nestjs/throttler";
 import {Public} from "./decorators/public.decorator";
+import { AuthGuard } from '@nestjs/passport';
 
 @Public()
 @Controller('auth')
@@ -13,8 +23,8 @@ export class AuthController {
   @Throttle({
     default: {
       limit: 5,
-      ttl: 60000
-    }
+      ttl: 60000,
+    },
   })
   @HttpCode(HttpStatus.OK)
   @Post('login')
@@ -25,13 +35,19 @@ export class AuthController {
   @Throttle({
     default: {
       limit: 5,
-      ttl: 60000
-    }
+      ttl: 60000,
+    },
   })
   @HttpCode(HttpStatus.CREATED)
   @Post('register')
   @Public()
   register(@Body() registerDto: RegisterDto) {
     return this.authService.register(registerDto);
+  }
+
+  @UseGuards(AuthGuard('jwt'))
+  @Get('me')
+  getMe(@Req() req: any) {
+    return req.user;
   }
 }
